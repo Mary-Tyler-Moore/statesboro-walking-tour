b4ed8f69d919944dde75d31fad3f9dadfc187414
Tyler Moore <tylermoore@Tylers-iMac.frontierlocal.net>
2020-02-24 13:58:33 -0500

    feat: create new quasar framework project

---
+++ src/pages/Index.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-full.svg"
+    >
+
+    <iframe src="https://booksy.com/widget/code.js?id=409055&country=us&lang=en">
+    </iframe>
+
+  </q-page>
+</template>
+
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
